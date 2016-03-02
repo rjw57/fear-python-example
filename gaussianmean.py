46590f05753bae7304336b1aa8edf532ce64eb16
R.J. Wareham <rjw57@fear.eng.cam.ac.uk>
2016-03-02 13:23:18 +0000

    Initial commit

---
+++ gaussianmean.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def main():
+    s = np.mean(np.random.randn(100))
+    print(s)
+
+if __name__ == '__main__':
+    main()
---
+++ run_gaussianmean.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#$ -S /bin/bash
+
+# Where is this script?
+DIR="${HOME}/python-example"
+
+# Change to that directory
+cd "${DIR}"
+
+# Run python script
+python ./gaussianmean.py
+
